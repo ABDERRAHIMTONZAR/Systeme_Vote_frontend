356222af187403d60e73efd01e2d7985573b1f25
Ali Belaouali <ali.belaouali@example.com>
2025-12-01 18:05:03 +0100

    liste polls

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Login from './pages/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateAccount from './pages/CreateAccount';
+import PollsList from './pages/PollsList';
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/create" element={<CreateAccount />} />
+          <Route path="/polls" element={<PollsList />} />
         </Routes>
       </BrowserRouter>
     
